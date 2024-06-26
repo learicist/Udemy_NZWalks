d70a3b741f578f3f0497eb18ea3ecb31ad3f94f2
Michael <learicist@gmail.com>
2024-06-26 10:11:54 -0400

    Delete Repositories directory

--- Repositories/ITokenRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace NZWalks.API.Repositories
-{
-    public interface ITokenRepository
-    {
-        string CreateJwtToken(IdentityUser user, List<string> roles);
-    }
-}
