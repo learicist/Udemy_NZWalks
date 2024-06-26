46617aee3829d67a1cb8233667fdfa530e155032
Michael <learicist@gmail.com>
2024-06-26 10:08:29 -0400

    Data, Mapper, Middle

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerId = "4fa8fd7e-2705-43a4-b115-d6b13a0b0771";
+            var writerId = "10272602-c87f-4206-a88f-b3b8624d685c";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerId,
+                    ConcurrencyStamp = readerId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerId,
+                    ConcurrencyStamp = writerId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
